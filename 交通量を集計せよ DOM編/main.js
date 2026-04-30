75b00c297fabe586c817b84439a9ade6f63e1551
ikeda042 <ikeda042main@gmail.com>
2026-04-30 12:38:37 +0900

    codeshare

---
+++ 交通量を集計せよ DOM編/main.js
@@ -0,0 +1,44 @@
+let walker = 0;
+let car = 0;
+let bike = 0;
+
+const walkerCounter = document.getElementById("walker-counter");
+const carCounter = document.getElementById("car-counter");
+const bikeCounter = document.getElementById("bike-counter");
+
+
+const walkerBtn = document.getElementById("walker-button");
+walkerBtn.addEventListener("click",onClickCallback1)
+
+const carBtn = document.getElementById("car-button");
+carBtn.addEventListener("click",onClickCallback2)
+
+const bikeBtn = document.getElementById("bike-button");
+bikeBtn.addEventListener("click",onClickCallback3)
+
+const reset = document.getElementById("reset-button");
+reset.addEventListener("click",onClickCallback4);
+
+function onClickCallback1(){
+  walker ++;
+  walkerCounter.textContent = walker;
+}
+
+function onClickCallback2(){
+  car ++;
+  carCounter.textContent = car;
+}
+
+function onClickCallback3(){
+  bike ++;
+  bikeCounter.textContent = bike;
+}
+
+function onClickCallback4(){
+  walker = 0;
+  car = 0;
+  bike = 0;
+  walkerCounter.textContent = 0;
+  carCounter.textContent = 0;
+  bikeCounter.textContent = 0
+}
