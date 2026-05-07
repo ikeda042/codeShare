5bc3a40e719f9202aa9849a6c3c38781fec5db93
Yunosuke Ikeda <ikeda042main@gmail.com>
2026-05-07 13:18:18 +0900

    Update challenge1.sql

--- sql/challenge1.sql
+++ sql/challenge1.sql
@@ -11,8 +11,10 @@ SET subject = "English" WHERE id = 4
 
 -- step4
 DELETE FROM exam_results WHERE teacher_name is NULL
+  
 -- step5
 SELECT id, name, subject FROM teachers WHERE subject IN ("Japanese", "math", "English")
+  
 -- step6
 SELECT student_name FROM exam_results 
 WHERE student_grade = 5 AND subject = "Japanese" 
@@ -21,7 +23,9 @@ LIMIT 5 OFFSET 5
 
 -- step7
 SELECT grade, count(id) AS each_grade FROM students GROUP BY grade
+  
 -- step8
 SELECT subject FROM teachers GROUP BY subject HAVING COUNT(subject) <= 6
+  
 -- step9
 SELECT student_name, SUM(score) AS total FROM exam_results GROUP BY student_name
