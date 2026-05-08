b025c3868763083e9b32eb0a53d61298eaae0753
Yunosuke Ikeda <ikeda042main@gmail.com>
2026-05-08 11:32:41 +0900

    Update book1.sql

--- sql/book1.sql
+++ sql/book1.sql
@@ -3,3 +3,9 @@ SELECT title FROM subjects
 WHERE NOT EXISTS (
 SELECT 1 FROM accounts 
 WHERE subject_id = subjects.id)
+
+--条件を満たす科目を抽出する 2
+SELECT title FROM subjects
+LEFT JOIN accounts ON accounts.subject_id = subjects.id
+GROUP BY subjects.title
+HAVING COUNT(accounts.id) < 3;
